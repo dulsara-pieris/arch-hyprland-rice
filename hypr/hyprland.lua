2713fa4681821b3cae635730d86e7119a417769f
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-09-03 18:37:06 +0530

    hypr

---
+++ hypr/hyprland.lua
@@ -0,0 +1,80 @@
+require("monitors")
+require("apps")
+require("animations")
+require("rules")
+require("decoration")
+
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+
+-----------------------
+---- LOOK AND FEEL ----
+-----------------------
+
+-- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
+
+hl.config({
+	dwindle = {
+		preserve_split = true,
+	},
+})
+
+hl.config({
+	master = {
+		new_status = "master",
+	},
+})
+
+hl.config({
+	scrolling = {
+		fullscreen_on_one_column = true,
+	},
+})
+
+----------------
+----  MISC  ----
+----------------
+
+hl.config({
+	misc = {
+		force_default_wallpaper = -1,
+		disable_hyprland_logo = false,
+	},
+})
+
+---------------
+---- INPUT ----
+---------------
+
+hl.config({
+	input = {
+		kb_layout = "us",
+		kb_variant = "",
+		kb_model = "",
+		kb_options = "",
+		kb_rules = "",
+
+		follow_mouse = 1,
+
+		sensitivity = 0,
+
+		touchpad = {
+			natural_scroll = false,
+		},
+	},
+})
+
+hl.gesture({
+	fingers = 3,
+	direction = "horizontal",
+	action = "workspace",
+})
+
+hl.device({
+	name = "epic-mouse-v1",
+	sensitivity = -0.5,
+})
